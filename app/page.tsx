1af23bfe71de3059e2ad69ab0756d4a5040bb018
Maxime ELIAZORD <mzozord@gmail.com>
2025-04-21 21:45:22 +0200

    Fixes results page reponsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,10 @@ export default function WeeklyPage() {
   }
 
   return (
-    <div className="py-6 px-20 space-y-4">
-      <h1 className="text-2xl font-bold">Analyses</h1>
+    <div className="py-6 px-4 sm:px-6 md:px-10 lg:px-20 space-y-4">
+      <h1 className="text-xl sm:text-2xl font-bold">Analyses</h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 sm:gap-6">
         {weekAnalysesRecords.map((record) => (
           <WeeklyAnalysisCard key={record.id} record={record} />
         ))}
